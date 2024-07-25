c1f27fb3cb8e260352033071cce3a1ee47a259bc
ChaitanyKakde <chaitnykakde517@gmail.com>
2024-07-25 09:46:44 +0530

    sign up page created

--- app/src/main/java/com/example/policebharti/SignupActivity.kt
+++ app/src/main/java/com/example/policebharti/SignupActivity.kt
@@ -8,26 +8,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
-    lateinit var tv_sign_up:TextView
-
+class SignupActivity : AppCompatActivity() {
+     lateinit var tv_login:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        tv_sign_up=findViewById(R.id.tv_sign_up)
-        tv_sign_up.setOnClickListener {
-            var intent= Intent(this,SignupActivity::class.java)
+        tv_login=findViewById(R.id.tv_login)
+        tv_login.setOnClickListener {
+            var intent= Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
-
-
-
 
     }
 }
