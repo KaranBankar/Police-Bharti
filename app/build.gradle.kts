812260d697dc03466240514461e291c70be58817
KaranBankar <karanbankar54@gmail.com>
2024-08-03 12:29:47 +0530

    Home Activity Designe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,9 @@ dependencies {
     //SDP dependecy for device compatable UI
     implementation("com.intuit.sdp:sdp-android:1.1.1")
 
+    //Material Cardview dependecy 
+    implementation("com.google.android.material:material:1.6.1")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
